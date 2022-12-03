a29742397db5e732b60430f7df54be910d9d4757
Weidaicheng <wdcdavyc@outlook.com>
2022-12-03 11:32:32 +0800

    remove password

--- client.sh
+++ client.sh
@@ -4,7 +4,5 @@ sed -i "s/SERVER_ADDR/${SERVER_ADDR}/g" client.json
 sed -i "s/SERVER_PORT/${SERVER_PORT}/g" client.json
 sed -i "s/CLIENT_UUID/${CLIENT_UUID}/g" client.json
 sed -i "s/ALTER_ID/${ALTER_ID}/g" client.json
-sed -i "s/USER/${USER}/g" client.json
-sed -i "s/PASS/${PASS}/g" client.json
 
 /usr/bin/v2ray run -c client.json
